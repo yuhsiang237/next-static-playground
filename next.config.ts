62851b6acb741710709add1586d9f3e6bf0eed74
YU HSIANG <umworksite@gmail.com>
2025-10-05 15:39:13 +0800

    feat: deploy configuration for a static GitHub Pages (#1)
    Co-authored-by: yuhsiang237 <12533553+yuhsiang237@users.noreply.github.com>

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/next-static-playground" : "",
+  assetPrefix: isProd ? "/next-static-playground/" : "",
 };
 
 export default nextConfig;
